126f22518e66513de77f08c50a25b561b33a707b
boyapatisaivenkat <65671588+boyapatisaivenkat@users.noreply.github.com>
2021-06-09 21:46:29 +0530

    Add files via upload

---
+++ AP19110010174_Boyapati Sai Venkat DAY-2 Task.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+string=input("Enter your roll number:-")
+if string[-3:].startswith('01'):
+    print("CSE Department")
+elif string[-3:].startswith('11'):
+    print("IT Department")
+elif string[-3:].startswith('21'):
+    print("ECE Department")
+else:
+    print("Roll Number not belongs to SRM University")
+
+
+# In[2]:
+
+
+string=input("Enter your roll number:-")
+if string[-3:].startswith('01'):
+    print("CSE Department")
+elif string[-3:].startswith('11'):
+    print("IT Department")
+elif string[-3:].startswith('21'):
+    print("ECE Department")
+else:
+    print("Roll Number not belongs to SRM University")
+
+
+# In[5]:
+
+
+string=input("Enter your roll number:-")
+if string[-3:].startswith('01'):
+    print("CSE Department")
+elif string[-3:].startswith('11'):
+    print("IT Department")
+elif string[-3:].startswith('21'):
+    print("ECE Department")
+else:
+    print("Roll Number not belongs to SRM University")
+
+
+# In[6]:
+
+
+string=input("Enter your roll number:-")
+if string[-3:].startswith('01'):
+    print("CSE Department")
+elif string[-3:].startswith('11'):
+    print("IT Department")
+elif string[-3:].startswith('21'):
+    print("ECE Department")
+else:
+    print("Roll Number not belongs to SRM University")
+
+
+# In[7]:
+
+
+string=input("Enter your roll number:-")
+if string[-3:].startswith('01'):
+    print("CSE Department")
+elif string[-3:].startswith('11'):
+    print("IT Department")
+elif string[-3:].startswith('21'):
+    print("ECE Department")
+else:
+    print("Roll Number not belongs to SRM University")
+
+
+# In[8]:
+
+
+string=input("Enter your roll number:-")
+if string[-3:].startswith('01'):
+    print("CSE Department")
+elif string[-3:].startswith('11'):
+    print("IT Department")
+elif string[-3:].startswith('21'):
+    print("ECE Department")
+else:
+    print("Roll Number not belongs to SRM University")
+
+
+# In[ ]:
+
+
+
+
